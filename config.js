d514f367968fcb0de017454604735d4f01ed6945
S-THA <95969180+S-THA@users.noreply.github.com>
2022-01-31 23:49:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,6 +13,6 @@ const CONFIG = {
     
     tha:'à năm mới toàn điểm A nhoa nhoa',
     nga:'Tha chúc nhiều dị gòy lên Sài Gòn nhớ lì xì Tha nha ,hehe',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Nhấp dô tiếp i (hứa không dỗi à) <3',
     Link:'https://s-tha.github.io/1505/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
